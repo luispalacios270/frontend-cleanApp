a90aa0938eeeb3ab2f8a9344bef0f282aea093fe
Carlos Vasquez <caed04@gmail.com>
2017-09-10 20:57:46 -0500

    General Changes

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -91,25 +91,19 @@ export class AboutPage {
     var myChart = new Chart(ctx, {
       type: 'pie',
       data: {
-        labels: ["Terminado", "Pendiente"/*, "Yellow", "Green", "Purple", "Orange"*/],
-        datasets: [{
-          /*label: '# of Votes',*/
+        labels: ["Terminado", "Pendiente"],
+        datasets: [{          
           data: [this.service.done, (this.service.total - this.service.done)],
           backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)'/*,
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'*/
+            // 'rgba(255, 99, 132, 0.2)',
+            'rgba(103, 143, 2, 0.2)',
+            'rgba(255, 99, 132, 0.2)'
+            // 'rgba(54, 162, 235, 0.2)'
           ],
           borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)'/*,
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'*/
+            'rgba(103, 143, 2, 1)',
+            'rgba(255, 99, 132, 1)'
+            // 'rgba(54, 162, 235, 1)'
           ],
           borderWidth: 1
         }]
