eded9312e115692641ffb31acd8e0cdafc153a89
Carlos Vasquez <caed04@gmail.com>
2017-12-12 19:16:13 -0500

    CHanged

--- src/pages/service-detail/service-detail.ts
+++ src/pages/service-detail/service-detail.ts
@@ -1,32 +1,34 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController, ModalController, ViewController, ToastController } from 'ionic-angular';
-import { ServicesProvider } from '../../providers/services/services';
-import { AreaPrividerProvider } from '../../providers/area-privider/area-privider';
-import { ClientProvider } from '../../providers/client/client';
-import { CreateUserPage } from '../create-user/create-user'
-import { TabsPage } from '../tabs/tabs';
-import { Storage } from '@ionic/storage';ƒ
-// import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
-import { File } from '@ionic-native/file';
-import { SocialSharing } from '@ionic-native/social-sharing';
+import { Component } from "@angular/core";
+import {
+  NavController,
+  NavParams,
+  AlertController,
+  ModalController,
+  ViewController,
+  ToastController
+} from "ionic-angular";
+import { ServicesProvider } from "../../providers/services/services";
+import { AreaPrividerProvider } from "../../providers/area-privider/area-privider";
+import { ClientProvider } from "../../providers/client/client";
+import { CreateUserPage } from "../create-user/create-user";
+import { TabsPage } from "../tabs/tabs";
+import { Storage } from "@ionic/storage";
 
-import * as globalVariables from '../../global'
+// import { File } from "@ionic-native/file";
+import { SocialSharing } from "@ionic-native/social-sharing";
 
-// @IonicPage()
+import * as globalVariables from "../../global";
+
 @Component({
-  selector: 'page-service-detail',
-  templateUrl: 'service-detail.html',
+  selector: "page-service-detail",
+  templateUrl: "service-detail.html"
 })
 export class ServiceDetailPage {
-
-  // auxList: Array<any>;
   service: any;
   areas: Array<any>;
   items: Array<any>;
   api: string = globalVariables.API_ENDPOINT;
-  selectedArea: string = '';
-  // fileTransfer: FileTransferObject = this.transfer.create();
-
+  selectedArea: string = "";
 
   constructor(
     public navCtrl: NavController,
@@ -38,34 +40,37 @@ export class ServiceDetailPage {
     public storage: Storage,
     public toastCtrl: ToastController,
     // private transfer: FileTransfer,
-    private file: File,
+    // private file: File,
     private socialSharing: SocialSharing
   ) {
-    this.service = navParams.get('service');
-    clientService.getClientAreas(this.service.client.id)
+    this.service = navParams.get("service");
+    clientService
+      .getClientAreas(this.service.client.id)
       .then((result: Array<object>) => {
         this.areas = result;
-      }).catch(err => console.log(err));
+      })
+      .catch(err => console.log(err));
   }
 
   editUser() {
-    // console.log(this.service.client);
-    this.navCtrl.push(CreateUserPage,
-      {
-        user: this.service.client
-      });
+    this.navCtrl.push(CreateUserPage, {
+      user: this.service.client
+    });
   }
 
   downloadPdf(service: any) {
     const url: string = `${this.api}/containers/pdf/download/${service.id}.pdf`;
-    this.socialSharing.share("Compartir PDF", null, url)
+    this.socialSharing
+      .share("Compartir PDF", null, url)
       .then()
       .catch(err => {
-        this.toastCtrl.create({
-          duration: 3000,
-          message: "El PDF no se encuentra generado aún",
-          position: "bottom"
-        }).present();
+        this.toastCtrl
+          .create({
+            duration: 3000,
+            message: "El PDF no se encuentra generado aún",
+            position: "bottom"
+          })
+          .present();
       });
   }
 
@@ -83,129 +88,50 @@ export class ServiceDetailPage {
         duration: 3000,
         message: "Hubo un error",
         position: "bottom"
-      }).present();      
+      }).present();
     });
   } */
 
   selectItem(item: object, areaName: string) {
-    this.storage.set("currentService", this.service.id)
+    this.storage
+      .set("currentService", this.service.id)
       .then(result => {
         console.log("Correcto");
-        this.storage.set("currentItem", item)
+        this.storage
+          .set("currentItem", item)
           .then(result => {
-            this.storage.set("areaName", areaName)
+            this.storage
+              .set("areaName", areaName)
               .then(result => {
                 this.navCtrl.push(TabsPage, {
                   navCtrl: this.navCtrl
                 });
               })
-              .catch(err => console.log(err))
-
+              .catch(err => console.log(err));
           })
           .catch(err => {
             console.log(err);
-          })
+          });
       })
       .catch(err => console.log(err));
   }
 
   selectArea(areaId: string, areaName: string) {
-    this.areaPrividerProvider.getItems(areaId)
+    this.areaPrividerProvider
+      .getItems(areaId)
       .then((result: Array<object>) => {
-        // console.log("items", result);
         if (result.length == 0) {
-          this.toastCtrl.create({
-            message: "El area seleccionado no cuenta con items",
-            duration: 3000,
-            position: 'bottom center'
-          }).present();
-
+          this.toastCtrl
+            .create({
+              message: "El area seleccionado no cuenta con items",
+              duration: 3000,
+              position: "bottom center"
+            })
+            .present();
         }
         this.items = result;
         this.selectedArea = areaName;
-        // this.auxList = this.items;
       })
       .catch(err => console.log(err));
   }
-
-
-
-  /*addArea() {
-    let alert = this.alertCtrl.create({
-      title: 'Nueva Área',
-      inputs: [
-        {
-          name: 'name',
-          placeholder: 'Nombre'
-        }
-      ],
-      buttons: [
-        {
-          text: 'Cancelar',
-          role: 'cancel',
-          handler: data => {
-            console.log('Cancel clicked');
-          }
-        },
-        {
-          text: 'Agregar',
-          handler: data => {
-            this.servicesProvider.createServiceArea(this.service.id, data)
-              .then(result => {
-                this.servicesProvider.getServicesAreas(this.service.id)
-                  .then((result: Array<object>) => {
-                    this.areas = result;
-                  })
-                  .catch(err => console.log(err));
-              });
-          }
-        }
-      ]
-    });
-    alert.present();
-  }
-
-  additem(areaId: string) {
-    let alert = this.alertCtrl.create({
-      title: 'Nuevo Item',
-      inputs: [
-        {
-          name: 'name',
-          placeholder: 'Nombre'
-        }
-      ],
-      buttons: [
-        {
-          text: 'Cancelar',
-          role: 'cancel',
-          handler: data => {
-            console.log('Cancel clicked');
-          }
-        },
-        {
-          text: 'Agregar',
-          handler: data => {
-            let newData: object = {
-              name: data.name,
-              qualification: 0,
-              finished: false,
-              areaId: areaId
-            }
-            this.areaPrividerProvider.createItems(areaId, newData)
-              .then(result => {
-                this.servicesProvider.getServicesAreas(this.service.id)
-                  .then((result: Array<object>) => {
-                    this.areas = result;
-                  })
-                  .catch(err => console.log(err));
-              });
-          }
-        }
-      ]
-    });
-    alert.present();
-
-
-  }*/
 }
-
