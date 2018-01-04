88facf62caf108bf72d5fda171a27fff29d9f5fe
Luis Eduardo Palacios Arboleda <lpalacios@Yuxis-MacBook-Pro-2.local>
2018-01-03 19:35:21 -0500

    Fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,51 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import { NgModule, ErrorHandler } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { IonicApp, IonicModule, IonicErrorHandler } from "ionic-angular";
+import { MyApp } from "./app.component";
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
-import { LoginPage } from '../pages/Login/login';
-import { ServiceDetailPage } from '../pages/service-detail/service-detail';
-import { ItemDetailPage } from '../pages/item-detail/item-detail';
-import { EditServicePage } from '../pages/edit-service/edit-service';
-import { NotesPage } from '../pages/notes/notes';
-import { AprobationPage } from '../pages/aprobation/aprobation';
-import { PicturePage } from '../pages/picture/picture';
-import { SignaturePage } from '../pages/signature/signature'
-import { CreateUserPage } from '../pages/create-user/create-user'
-import { CreateAreasPage } from '../pages/create-areas/create-areas'
-import { CreateItemPage } from '../pages/create-item/create-item'
+import { AboutPage } from "../pages/about/about";
+import { ContactPage } from "../pages/contact/contact";
+import { HomePage } from "../pages/home/home";
+import { TabsPage } from "../pages/tabs/tabs";
+import { LoginPage } from "../pages/Login/login";
+import { ServiceDetailPage } from "../pages/service-detail/service-detail";
+import { ItemDetailPage } from "../pages/item-detail/item-detail";
+import { EditServicePage } from "../pages/edit-service/edit-service";
+import { NotesPage } from "../pages/notes/notes";
+import { AprobationPage } from "../pages/aprobation/aprobation";
+import { PicturePage } from "../pages/picture/picture";
+import { SignaturePage } from "../pages/signature/signature";
+import { CreateUserPage } from "../pages/create-user/create-user";
+import { CreateAreasPage } from "../pages/create-areas/create-areas";
+import { CreateItemPage } from "../pages/create-item/create-item";
 
-import { IonicStorageModule } from '@ionic/storage';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { UserServiceProvider } from '../providers/user-service/user-service';
-import { HttpModule } from '@angular/http';
-import { ServicesProvider } from '../providers/services/services';
-import { AreaPrividerProvider } from '../providers/area-privider/area-privider';
-import { ItemProvider } from '../providers/item/item';
-import { NotePrividerProvider } from '../providers/note-privider/note-privider';
-import { FilesProvider } from '../providers/files/files';
-import { ValidationProvider } from '../providers/validation/validation';
-import { Toast } from '@ionic-native/toast';
-import { Dialogs } from '@ionic-native/dialogs';
-import { ClientProvider } from '../providers/client/client';
-import { Camera } from '@ionic-native/camera';
-import { SignaturePadModule } from 'angular2-signaturepad';
-import { ItemInspectionProvider } from '../providers/item-inspection/item-inspection';
-import { UrlPicDirective } from '../directives/url-pic/url-pic';
+import { IonicStorageModule } from "@ionic/storage";
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { UserServiceProvider } from "../providers/user-service/user-service";
+import { HttpModule, Http } from "@angular/http";
+import { ServicesProvider } from "../providers/services/services";
+import { AreaPrividerProvider } from "../providers/area-privider/area-privider";
+import { ItemProvider } from "../providers/item/item";
+import { NotePrividerProvider } from "../providers/note-privider/note-privider";
+import { FilesProvider } from "../providers/files/files";
+import { ValidationProvider } from "../providers/validation/validation";
+import { Toast } from "@ionic-native/toast";
+import { Dialogs } from "@ionic-native/dialogs";
+import { ClientProvider } from "../providers/client/client";
+import { Camera } from "@ionic-native/camera";
+import { SignaturePadModule } from "angular2-signaturepad";
+import { ItemInspectionProvider } from "../providers/item-inspection/item-inspection";
+import { UrlPicDirective } from "../directives/url-pic/url-pic";
+import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 
+// import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { SocialSharing } from "@ionic-native/social-sharing";
 
-import { SocialSharing } from '@ionic-native/social-sharing';
+export function createTranslateLoader(http: Http) {
+  return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
+}
 
 @NgModule({
   declarations: [
@@ -60,13 +66,19 @@ import { SocialSharing } from '@ionic-native/social-sharing';
     CreateAreasPage,
     UrlPicDirective,
     CreateItemPage
-
   ],
   imports: [
     BrowserModule,
     HttpModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [Http]
+      }
+    }),
     IonicModule.forRoot(MyApp, {
-      menuType: 'overlay'
+      menuType: "overlay"
     }),
     IonicStorageModule.forRoot(),
     SignaturePadModule
@@ -109,7 +121,6 @@ import { SocialSharing } from '@ionic-native/social-sharing';
     FilesProvider,
     ItemInspectionProvider,
     SocialSharing
-
   ]
 })
-export class AppModule { }
+export class AppModule {}
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { NavController, NavParams, ToastController, LoadingController } from 'ionic-angular';
-import { HomePage } from '../home/home';
-import { UserServiceProvider } from '../../providers/user-service/user-service';
-import { Storage } from '@ionic/storage';
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import {
+  NavController,
+  NavParams,
+  ToastController,
+  LoadingController
+} from "ionic-angular";
+import { HomePage } from "../home/home";
+import { UserServiceProvider } from "../../providers/user-service/user-service";
+import { Storage } from "@ionic/storage";
 
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+  selector: "page-login",
+  templateUrl: "login.html"
 })
 export class LoginPage {
-
   // Object to validate if the user is in the inputs.
   try: {} = {
     email: false,
@@ -30,12 +34,12 @@ export class LoginPage {
   user: any = {
     email: "",
     password: ""
-  }
+  };
 
   loader: any = this.loadingCtrl.create({
-    content: "Cargando"/* ,
+    content: "Cargando" /* ,
       duration: 3000 */
-  });/*  = this.loadingCtrl.create({
+  }); /*  = this.loadingCtrl.create({
     content: "Cargando"
   }); */
 
@@ -49,22 +53,35 @@ export class LoginPage {
     public storage: Storage
   ) {
     this.loginForm = FormBuilder.group({
-      email: ['', Validators.compose([Validators.maxLength(100), this.emailValidator,/* Validators.pattern("/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/")*/, Validators.required])],
-      password: ['', Validators.required]
+      email: [
+        "",
+        Validators.compose([
+          Validators.maxLength(100),
+          this
+            .emailValidator /* Validators.pattern("/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/")*/,
+          ,
+          Validators.required
+        ])
+      ],
+      password: ["", Validators.required]
     });
   }
 
   /**
-   * Validate if the value is a valid email thru regExp   
-   * @param {any} control value of input   
+   * Validate if the value is a valid email thru regExp
+   * @param {any} control value of input
    * @returns {object} returns an object with the attribute invalidEmail true if there is an error
    */
   emailValidator(control: any) {
     // RFC 2822 compliant regex
-    if (control.value.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)) {
+    if (
+      control.value.match(
+        /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+      )
+    ) {
       return null;
     } else {
-      return { 'invalidEmail': true };
+      return { invalidEmail: true };
     }
   }
 
@@ -76,7 +93,6 @@ export class LoginPage {
     this.loader.dismiss();
   }
 
-
   login() {
     // this.navCtrl.push(HomePage);
     let emailTrimmed: string = this.user.email;
@@ -86,21 +102,25 @@ export class LoginPage {
       content: "Cargando"
     });
     loader.present();
-    this.userServiceProvider.doLogin(this.user).then((result: any) => {
-      console.log(result);
-      loader.dismiss();
-      this.storage.set("currentUser", result.userId);
-      this.navCtrl.push(HomePage);
-    }).catch(err => {
-      loader.dismiss();
-      if (err.name === 'TimeoutError') {
-        this.toast.create({
-          message: 'El servidor no esta disponible',
-          duration: 3000,
-          position: 'bottom'
-        }).present();
-      } else
-        this.wrongUser = true;
-    });
+    this.userServiceProvider
+      .doLogin(this.user)
+      .then((result: any) => {
+        console.log(result);
+        loader.dismiss();
+        this.storage.set("currentUser", result.userId);
+        this.navCtrl.push(HomePage);
+      })
+      .catch(err => {
+        loader.dismiss();
+        if (err.name === "TimeoutError") {
+          this.toast
+            .create({
+              message: "El servidor no esta disponible",
+              duration: 3000,
+              position: "bottom"
+            })
+            .present();
+        } else this.wrongUser = true;
+      });
   }
 }
