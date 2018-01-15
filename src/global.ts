e6715168dfdfbc1205f79b0bf9f4c97da7b33ebe
Carlos Vasquez <caed04@gmail.com>
2018-01-15 07:09:51 -0500

    Fix logo

--- src/global.ts
+++ src/global.ts
@@ -1,7 +1,7 @@
 //  File with global constants
 
 // Development API_ENDPOINT.
-export const API_ENDPOINT: string = 'http://localhost:3000/api';
+// export const API_ENDPOINT: string = 'http://localhost:3000/api';
 
 // Test API_ENDPOINT.
-// export const API_ENDPOINT: string = 'https://limpiezapp.herokuapp.com/api';
+export const API_ENDPOINT: string = 'https://limpiezapp.herokuapp.com/api';
